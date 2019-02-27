a1786a5339ca6a637e6aaafcd2c71cca0b961f19
Akshay <akshaysrikumar@gmail.com>
2019-02-27 08:26:13 -0600

    C#

---
+++ ExceptionEasy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExceptionEasy
+{
+    class MainClass
+    {
+        public class Student
+        {
+
+            public string StudentName { get; set; }
+        }
+        public static void Main(string[] args)
+        {
+            Student std = null;
+
+            try
+            {
+                try
+                {
+           
+                    std.StudentName = "";
+                }
+                catch (InvalidOperationException innerEx)
+                {
+                    Console.WriteLine("Inner catch");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Outer catch");
+            }
+        }
+    }
+}
